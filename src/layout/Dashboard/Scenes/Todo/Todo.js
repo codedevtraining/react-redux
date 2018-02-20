1a936bbb7a2b787c3ec5bdd6e704045d3ba9e739
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-20 11:45:31 -0300

    feat: set props in children components and set axios methods.

--- src/layout/Dashboard/Scenes/Todo/Todo.js
+++ src/layout/Dashboard/Scenes/Todo/Todo.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Todo = () => {
-  return (
-    <div>
-      <h1>Todo</h1>
-    </div>
-  );
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { API_ENDPOINT } from '../../../../environments/environmnets';
+
+class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${API_ENDPOINT}/tasks?sort=-description`)
+      .then(res => this.setState({ 
+        ...this.state,
+        description:'',
+        list: res.data
+      }))
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+
+  handleAdd() {
+    const description = this.state.description
+    axios.post(`${API_ENDPOINT}/tasks`, { description })
+      .then(res => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${API_ENDPOINT}/tasks/${todo.id}`)
+      .then(res => this.refresh())
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo</h1>
+        <TodoForm 
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
+        <TodoList 
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+        />
+      </div>
+    );
+  }
 };
 
 export default Todo;
